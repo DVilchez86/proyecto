e0e12e2099d8eaf0b4259652449d653e9a5234bb
DVilchez86 <159927067+DVilchez86@users.noreply.github.com>
2024-04-19 08:49:38 +0200

    Create Objeto.java

---
+++ app/src/main/java/com/example/proyectodam/models/Objeto.java
@@ -0,0 +1,26 @@
+package com.example.proyectodam.models;
+
+public class Objeto {
+    //Atributos de campo:
+    private String descripcion;
+    private int id;
+
+    //Setters y getters:
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
